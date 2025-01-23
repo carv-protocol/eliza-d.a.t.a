073b6573ed95e56ab3b46d644f2e51f25deae358
pis <piscarefree@gmail.com>
2025-01-23 18:55:06 +0800

    chore: tokenInfo optimize

--- packages/plugin-d.a.t.a/src/index.ts
+++ packages/plugin-d.a.t.a/src/index.ts
@@ -2,12 +2,13 @@ import { Plugin } from "@elizaos/core";
 import { fetchTransactionAction } from "./actions/fetchTransaction";
 import { fetchTokenInfoAction } from "./actions/fetchTokenInfo";
 import { ethereumDataProvider } from "./providers/ethereum/database";
-import { transferAction } from "@elizaos/plugin-evm";
+import { tokenInfoProvider } from "./providers/token/tokenInfo";
+
 export const onchainDataPlugin: Plugin = {
     name: "onchain data plugin",
     description: "Enables onchain data fetching",
-    actions: [fetchTokenInfoAction, fetchTransactionAction],
-    providers: [],
+    actions: [],
+    providers: [tokenInfoProvider, ethereumDataProvider],
     evaluators: [],
     services: [],
     clients: [],
